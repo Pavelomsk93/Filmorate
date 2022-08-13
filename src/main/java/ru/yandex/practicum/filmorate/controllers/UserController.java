559fa94c8a2c18a6a0df5fa7d10ffb3425704000
Pavel <pavelomsk93@yandex.ru>
2022-08-13 15:54:03 +0600

    <Refactor>:<добавить методы>

--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -10,7 +10,7 @@ import ru.yandex.practicum.filmorate.service.user.UserService;
 
 import javax.validation.Valid;
 import java.util.List;
-
+import java.util.Set;
 
 
 @RestController
@@ -43,4 +43,26 @@ public class UserController {
     public User findBId(@PathVariable int id){
         return userService.findById(id);
     }
+
+    @PutMapping("/{id}/friends/{friendId}")
+    public Set<Integer> addFriends(@PathVariable int id, @PathVariable int friendId){
+        return userService.addFriend(id,friendId);
+    }
+
+    @DeleteMapping("/{id}/friends/{friendId}")
+    public Set<Integer> deleteFriends(@PathVariable int id, @PathVariable int friendId){
+        return userService.removeFriend(id,friendId);
+    }
+
+    @GetMapping("/{id}/friends")
+    public Set<Integer> getAllFriends(@PathVariable int id){
+        return userService.getAllFriends(id);
+    }
+
+    @GetMapping("/{id}/friends/common/{otherId}")
+    public Set<Integer> commonFriends(@PathVariable int id,@PathVariable int otherId){
+        return userService.commonFriends(id,otherId);
+    }
+
+
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
@@ -1,13 +1,18 @@
 package ru.yandex.practicum.filmorate.service.user;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exceptions.UserNotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
+@Slf4j
 public class UserService {
 
     UserStorage userStorage;
@@ -33,7 +38,29 @@ public class UserService {
         return userStorage.findById(id);
     }
 
-    public void addFriend(int id,int friendId){
-        userStorage.findById(id);
+    public Set<Integer> addFriend(int id, int friendId){
+        userStorage.findById(id).getFriends().add(friendId);
+        userStorage.findById(friendId).getFriends().add(id);
+        return userStorage.findById(id).getFriends();
+    }
+
+    public Set<Integer> removeFriend(int id, int friendId){
+       if(userStorage.findById(id).getFriends().contains(friendId)){
+           userStorage.findById(id).getFriends().remove(friendId);
+       }else{
+           log.error("Такого пользователя нет в друзьях");
+           throw new UserNotFoundException("Такого пользователя нет в друзьях");
+       }
+       return userStorage.findById(id).getFriends();
+    }
+
+    public Set<Integer> getAllFriends(int id){
+        return userStorage.findById(id).getFriends();
+    }
+
+    public Set<Integer> commonFriends(int id,int otherId){
+        Set<Integer> common = new HashSet<>(userStorage.findById(otherId).getFriends());
+        common.retainAll(userStorage.findById(id).getFriends());
+        return common;
     }
 }
